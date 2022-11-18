d476f67fa3f1edaf0d02f3cf18ccda2a294c1df2
VladSalash <salashchenko99@gmail.com>
2022-11-18 10:54:33 +0300

    fix

--- src/components/Review/ReviewCard.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Text = styled.span`
-  padding-left: 20px;
-`;
-
-export const CastItem = styled.li`
-  width: 200px;
-`;
--- src/components/SearchMovie/SearchMovies.styled.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const MovieList = styled.ul`
-  margin-top: 15px;
-`;
